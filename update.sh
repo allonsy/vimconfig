eef3c4bbfa8ade75f38fac08a87c94f93b14e930
Alec Snyder <linuxbash8@gmail.com>
2019-04-27 01:50:42 -0700

    add vim surround and fix update script

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+PACKAGE_NAME='*'
+if [ $# -ge 1 ]; then
+    PACKAGE_NAME=$1
+fi
+
+function update_package() {
+    echo "Updating: $1"
+    cd $1
+    git pull origin master
+    cd ..
+}
+    
+cd ~/.vim/pack/allonsy/start
+if [ "$PACKAGE_NAME" = '*' ]; then
+    echo "Updating all packages"
+    for package in `ls`; do
+        update_package $package
+    done
+else
+    update_package $1
+fi
