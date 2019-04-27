cc1681fd2ca06adefdc3f0b9681f6f803eb9b4a1
Alec Snyder <linuxbash8@gmail.com>
2019-04-27 01:32:11 -0700

    Add delete script

---
+++ delete.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 1]; then
+    echo "Please provide a package to remove"
+    exit 1
+fi
+
+cd ~/.vim/pack/allonsy/start
+git submodule deinit -f -- $1
+rm -rf ~/.vim/.git/modules/pack/allonsy/$1
+git rm -f $1
